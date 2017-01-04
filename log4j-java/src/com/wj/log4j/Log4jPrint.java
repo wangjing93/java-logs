1a34ae23801eae68b48ddb1302906d99588b0d36
wangjing <jing.wang@daigj.com>
2017-01-04 19:25:20 +0800

    add log4j jar log4j.properties

--- log4j-java/src/com/wj/log4j/Log4jPrint.java
+++ log4j-java/src/com/wj/log4j/Log4jPrint.java
@@ -1,7 +1,19 @@
 package com.wj.log4j;
 
+import org.apache.log4j.Logger;
+
 /**
  * Created by WangJing on 2017/1/4.
  */
 public class Log4jPrint {
+
+    private static Logger logger = Logger.getLogger(Log4jPrint.class);
+
+    public static void main(String[] args) {
+        System.out.println("打印system out");
+
+        logger.debug("debug msg");
+        logger.info("info msg");
+        logger.error("error msg");
+    }
 }
